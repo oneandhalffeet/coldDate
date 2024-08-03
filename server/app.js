ac0571a05a2ee9d2635a826afd4efffb0f65effc
oneandhalfeet <uddeshteke007@gmail.com>
2024-08-03 11:54:32 +0530

    created nextjs folder

--- server/app.js
+++ server/app.js
@@ -20,6 +20,14 @@ app.get('/', (req, res) => {
     res.send("Hello World!");
 });
 
+app.get('/login', (req, res) => {
+    res.send("Login");
+});
+
+app.get('/register', (req, res) => {
+    res.send("Register");
+});
+
 app.get('/index', (req, res) => {
     console.log("index");
     res.send("Hello Index!");
